14fb2e98d314dba524ecbc4c9685932f5e1ffde5
Rajeev Atla <rajeev@rajeevatla.com>
2021-04-20 20:39:38 -0400

    removus columns

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import pandas as pd
 URL = 'https://docs.google.com/spreadsheets/d/1uuGMcZCRLYsdcoRp13unie8BIEuEUvjC_R-hRpCUVdA/edit#gid=1038305874' #URL for data
 URL = URL.replace('/edit#gid=', '/export?format=csv&gid=') #Have gsheets convert it into a csv
 
-data = pd.read_csv(URL) #point Pandas to the data
+names = ['Timestamp', 'Email Address', 'Social Media', '#1', '#2', '#3', '#4', '#5', '#6']
+data = pd.read_csv(URL, header = 0, names = names) #point Pandas to the data
 emails = data.pop('Email Address') #remove emails from data
+timestamp = data.pop('Timestamp')
 
-n1, n2 = len(data.index), len(data.columns) #get number of respondents and number of questions
-df = (n1 - 1) * (n2 - 1) #degrees of freedom
+respondents, questions = len(data.index), len(data.columns)-1 #get number of respondents and number of questions
+print(data)
